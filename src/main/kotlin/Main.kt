4f5d5eeeb74f9f72a99b462336ceae252b6489a4
rebok dev <68105407+rebokdev@users.noreply.github.com>
2023-02-28 22:33:06 +0100

    0.0.3 With support for arguments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,12 @@ val Options = mapOf<String,Option>("help" to Option(::help,"Shows help", listOf(
 var Usage: String = ""
 var index: Int = 0
 fun main(args: Array<String>) {
-    IntegrityCheck(*Options.values.toTypedArray())
+    for (it in Options.values) {
+        it.IntegrityCheck()
+    }
         if (args.isNotEmpty()) {
             if (Options.containsKey(args[0])) {
-                Options[args[0]]?.func?.call()
+                Options[args[0]]?.run(args)
             } else {
                 println("There's no such an option")
             }
@@ -18,13 +20,14 @@ fun main(args: Array<String>) {
 
 }
 
-fun about() {
+fun about(Parameters: ParameterPack) {
     println("FENIX - Package manager built to be fast")
     println()
     println("version: 0.0.2")
 }
 
-fun help() {
+
+fun help(Parameters: ParameterPack) {
     println("HELP")
     for (it in Options.keys) {
         println()
@@ -47,22 +50,69 @@ fun help() {
     }
 }
 
-fun install() {
+fun install(Parameters: ParameterPack) {
     TODO()
 }
 
-fun remove() {
+fun remove(Parameters: ParameterPack) {
     TODO()
 }
 
-data class Option(val func: KFunction<Unit>,val description: String,val arg_types: List<String>,val arg_names: List<String>)
+data class Option(val func: KFunction<Unit>,val description: String,val arg_types: List<String>,val arg_names: List<String>) {
+    fun run(arguments: Array<String>) {
+        var Parameters: MutableList<String> = mutableListOf()
+        var AdditionalParameters: MutableMap<String,String> = mutableMapOf()
+        for (it in arguments) {
+            if (it != arguments[0]) {
+                if (it[0] == '-' && it[1] == '-') {
+                    if (it.contains('=')) {
+                        AdditionalParameters.put(it.substring(2).split('=')[0],it.split('=')[1])
 
-fun IntegrityCheck(vararg Options: Option) {
-    for (it in Options) {
-        if (it.arg_types.size != it.arg_names.size) {
-            throw Exception("Option data is not completed.")
+                    } else {
+                        AdditionalParameters.put(it.substring(2),"true")
+                    }
+
+                } else {
+                    Parameters.add(it)
+                }
+            }
+        }
+
+        var indexOfArg = 0
+        var IntList: MutableList<Int> = mutableListOf()
+        var DoubleList: MutableList<Double> = mutableListOf()
+        var StringList: MutableList<String> = mutableListOf()
+        for (it in arg_types) {
+            when (it) {
+                "Int" -> IntList.add(Parameters[indexOfArg].toInt())
+                "Double" -> DoubleList.add(Parameters[indexOfArg].toDouble())
+                "String" -> StringList.add(Parameters[indexOfArg].toString())
+            }
+        }
+        func.call(ParameterPack(arg_types,IntList,DoubleList,StringList,AdditionalParameters))
+    }
+
+    fun IntegrityCheck() {
+            if (arg_types.size != arg_names.size) {
+                throw Exception("Option data is not completed.")
+            }
+
+    }
+
+}
+
+data class ParameterPack(val arg_types: List<String>,val IntList: List<Int>, val DoubleList: List<Double>, val StringList: List<String>, val AdditionalParameters: Map<String, String>) {
+    fun get(index: Int): String {
+        return when(arg_types[index]) {
+            "Int" -> IntList[index].toString()
+            "Double" -> DoubleList[index].toString()
+            "String" -> StringList[index]
+            else -> "NoArg"
         }
     }
 
+    fun getAdditionalParameter(indexOfAdditionalParameter: String): String? {
+        return AdditionalParameters[indexOfAdditionalParameter]
+    }
 }
 
