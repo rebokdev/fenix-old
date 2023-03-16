eec2d142e2316e1e7ec837b94336845a6a824653
rebok dev <68105407+rebokdev@users.noreply.github.com>
2023-03-16 23:37:34 +0100

    0.04 With some fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 import kotlin.reflect.KFunction
 
-val Options = mapOf<String,Option>("help" to Option(::help,"Shows help", listOf(), listOf()),"about" to Option(::about,"Shows about message",
-    listOf(), listOf()))
+val Options = mutableMapOf<String,Option>("help" to Option(::help,"Shows help", listOf(), listOf(), listOf(), mapOf()),"about" to Option(::about,"Shows about message",
+    listOf(), listOf(), listOf(), mapOf()), "install" to Option(::install,"Installs local package or remote one", listOf("String"), listOf("package name"), listOf(), mapOf())
+)
 var Usage: String = ""
 var index: Int = 0
 fun main(args: Array<String>) {
@@ -40,7 +41,7 @@ fun help(Parameters: ParameterPack) {
         Usage = "    Usage: ${it} "
         Options[it]?.arg_names?.let { it1 ->
             repeat(it1.size) {
-                Usage = "[${it1[index]}]"
+                Usage += "[${it1[index]}]"
                 index++
 
             }
@@ -58,7 +59,14 @@ fun remove(Parameters: ParameterPack) {
     TODO()
 }
 
-data class Option(val func: KFunction<Unit>,val description: String,val arg_types: List<String>,val arg_names: List<String>) {
+data class Option(
+    val func: KFunction<Unit>,
+    val description: String,
+    val arg_types: List<String>,
+    val arg_names: List<String>, val PossibleParams: List<Any>,
+    val AvailableAdditionalParams: Map<String,String>
+) {
+
     fun run(arguments: Array<String>) {
         var Parameters: MutableList<String> = mutableListOf()
         var AdditionalParameters: MutableMap<String,String> = mutableMapOf()
@@ -66,10 +74,10 @@ data class Option(val func: KFunction<Unit>,val description: String,val arg_type
             if (it != arguments[0]) {
                 if (it[0] == '-' && it[1] == '-') {
                     if (it.contains('=')) {
-                        AdditionalParameters.put(it.substring(2).split('=')[0],it.split('=')[1])
+                        AdditionalParameters[it.substring(2).split('=')[0]] = it.split('=')[1]
 
                     } else {
-                        AdditionalParameters.put(it.substring(2),"true")
+                        AdditionalParameters[it.substring(2)] = "true"
                     }
 
                 } else {
@@ -82,37 +90,99 @@ data class Option(val func: KFunction<Unit>,val description: String,val arg_type
         var IntList: MutableList<Int> = mutableListOf()
         var DoubleList: MutableList<Double> = mutableListOf()
         var StringList: MutableList<String> = mutableListOf()
+        var IDs: MutableList<ID> = mutableListOf()
         for (it in arg_types) {
-            when (it) {
+            when (arg_types[indexOfArg]) {
+                "Int" -> IDs.add(ID(listOf(indexOfArg,1)))
+                "Double" -> IDs.add(ID(listOf(indexOfArg,2)))
+                "String" -> IDs.add(ID(listOf(indexOfArg,2)))
+                else -> "arg type is not correct"
+            }
+            when (arg_types[indexOfArg]) {
                 "Int" -> IntList.add(Parameters[indexOfArg].toInt())
                 "Double" -> DoubleList.add(Parameters[indexOfArg].toDouble())
-                "String" -> StringList.add(Parameters[indexOfArg].toString())
+                "String" -> StringList.add(Parameters[indexOfArg])
+                else -> "arg type is not correct"
+            }
+            index++
+        }
+
+        var AdditionalParamsIDs: MutableMap<String, ID> = mutableMapOf()
+        var IntAdditionalParams: MutableMap<String, Int> = mutableMapOf()
+        var DoubleAdditionalParams: MutableMap<String, Double> = mutableMapOf()
+        var StringAdditionalParams: MutableMap<String, String> = mutableMapOf()
+        var BoolAdditionalParams: MutableMap<String, Boolean> = mutableMapOf()
+
+        for (it in AvailableAdditionalParams) {
+            when (it.key) {
+                "Int" -> AdditionalParamsIDs.put(it.key,ID(listOf(1)))
+                "Double" -> AdditionalParamsIDs.put(it.key,ID(listOf(2)))
+                "String" -> AdditionalParamsIDs.put(it.key,ID(listOf(3)))
+                "Boolean" -> AdditionalParamsIDs.put(it.key,ID(listOf(4)))
+                else -> println("")
+            }
+            when (AdditionalParamsIDs[it.key]?.get()?.get(0)) {
+                1 -> IntAdditionalParams.put(it.key,it.value.toInt())
+                2 -> DoubleAdditionalParams.put(it.key,it.value.toDouble())
+                3 -> StringAdditionalParams.put(it.key,it.value)
+                4 -> BoolAdditionalParams.put(it.key,it.value.toBoolean())
             }
         }
-        func.call(ParameterPack(arg_types,IntList,DoubleList,StringList,AdditionalParameters))
+
+        func.call(ParameterPack(IDs,arg_types,IntList,DoubleList,StringList,AdditionalParameters,AdditionalParamsIDs,IntAdditionalParams,
+            DoubleAdditionalParams,StringAdditionalParams,BoolAdditionalParams))
     }
 
     fun IntegrityCheck() {
-            if (arg_types.size != arg_names.size) {
-                throw Exception("Option data is not completed.")
+        if (arg_types.size != arg_names.size) {
+            throw Exception("Option data is not completed.")
+        }
+
+        for (it in arg_types) {
+            if (it != "Int" && it != "Double" && it != "String") {
+                throw Exception("arg type \"${it}\" is not correct. At function: ${func.name}")
             }
+        }
+
+        for (it in AvailableAdditionalParams.keys) {
+            if (it != "Int" && it != "Double" && it != "String" && it != "Boolean") {
+                throw Exception("Addiyional parameter type \"${it}\" is not correct. At function: ${func.name}")
+            }
+        }
+
 
     }
 
 }
 
-data class ParameterPack(val arg_types: List<String>,val IntList: List<Int>, val DoubleList: List<Double>, val StringList: List<String>, val AdditionalParameters: Map<String, String>) {
+data class ParameterPack(val IDs: List<ID>,
+                         val arg_types: List<String>, val IntList: List<Int>, val DoubleList: List<Double>,
+                         val StringList: List<String>, val AdditionalParams: Map<String,String> ,val AdditionalParamsIDs: Map<String, ID>,
+                         val IntAdditionalParams: Map<String, Int>, val DoubleAdditionalParams: Map<String, Double>, val StringAdditionalParams: Map<String,String>,
+                         val BoolAdditionalParams: Map<String, Boolean>) {
+
     fun get(index: Int): String {
         return when(arg_types[index]) {
-            "Int" -> IntList[index].toString()
-            "Double" -> DoubleList[index].toString()
-            "String" -> StringList[index]
+            "Int" -> IntList[IDs[index].get()[0]].toString()
+            "Double" -> DoubleList[IDs[index].get()[0]].toString()
+            "String" -> StringList[IDs[index].get()[0]]
             else -> "NoArg"
         }
     }
 
-    fun getAdditionalParameter(indexOfAdditionalParameter: String): String? {
-        return AdditionalParameters[indexOfAdditionalParameter]
+
+    fun <T>getAdditionalParameter(indexOfAdditionalParameter: String): Comparable<*>? {
+        return when ((AdditionalParamsIDs[indexOfAdditionalParameter]?.get()?.get(0))) {
+            1 -> IntAdditionalParams[indexOfAdditionalParameter]
+            2 -> DoubleAdditionalParams[indexOfAdditionalParameter]
+            3 -> StringAdditionalParams[indexOfAdditionalParameter]
+            4 -> BoolAdditionalParams[indexOfAdditionalParameter]
+            else -> "NoParam"
+        }
     }
 }
-
+data class ID(val Numbers: List<Int> ) {
+    fun get(): List<Int> {
+        return Numbers
+    }
+}
